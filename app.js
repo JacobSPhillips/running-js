28b41a0b505fb0f88d438fcc8bcca9c553f60bc5
Jacob Phillips <jsp452@msstate.edu>
2024-06-26 07:49:25 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("your number is less than 0.5");
+//     console.log(random);
+// }
+
+// if (random >= 0.5) {
+//     console.log("your number is greater than 0.5");
+//     console.log(random);
+// }
+
+// const dayOfWeek = "friday";
+
+// if (dayOfWeek === "monday") {
+//     console.log("UGH i hate mondays")
+// } else if (dayOfWeek === "saturday") {
+//     console.log("YAY i love saturdays")
+// } else if (dayOfWeek === "friday") {
+//     console.log("I really like fridays")
+// }
+
+// let age = 9;
+
+// if (age <= 5) {
+//     console.log("You get in free")
+// } else if (age <= 10) {
+//     console.log("You cost 10$")
+// } else if (age <= 65) {
+//     console.log("You are a adult. 20$")
+// } else {
+//     console.log("You are basically an wrinkly child. 10$")
+// }
+
+
+// const password = prompt("Plase enter a new password");
+
+// if (password.length >= 6) {
+//     console.log("LONG ENOUGH PASSWORD")
+// } else {
+//     console.log("PASSWORD TOO SHORT MUST BE 6 CHARACTERS")
+// }
+
+
+// if (password.indexOf(" ") === -1) {
+//     console.log("good job! no space")
+// } else {
+//     console.log("password cannot contain spaces")
+// }
+
+const day = 5;
+switch (day) {
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY")
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    case 4:
+        console.log("THURSDAY")
+        break;
+    case 5:
+        console.log("FRDAY")
+        break;
+    default:
+        console.log("I don't know that")
+}
